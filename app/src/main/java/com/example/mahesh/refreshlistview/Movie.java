1a6188e86857e043f29fb39eedbb68bdecce6318
MAHESH <mahesh0419@gmail.com>
2015-12-10 14:41:47 -0800

    Initial commit

---
+++ app/src/main/java/com/example/mahesh/refreshlistview/Movie.java
@@ -0,0 +1,17 @@
+package com.example.mahesh.refreshlistview;
+
+/**
+ * Created by MAHESH on 11/20/2015.
+ */
+public class Movie {
+    public int id;
+    public String title;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+}
